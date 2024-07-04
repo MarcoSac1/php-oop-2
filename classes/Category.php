2c51609cf3f76cb4f5696ac32ffdc0ff1cb91136
MarcoSac1 <marcosaccone8@gmail.com>
2024-07-04 17:20:44 +0200

    created class Category and Product

--- classes/Category.php
+++ classes/Category.php
@@ -1,5 +1,20 @@
 <?php
 
 class Category{
-    
+    private $name;
+    private $description;
+
+    public function __construct($name, $description){
+        $this -> name = $name;
+        $this -> description = $description;
+    }
+
+    public function getName(){
+        return $this -> name;
+    }
+
+    public function getDescription(){
+        return $this -> description;
+    }
+
 }
--- classes/Product.php
+++ classes/Product.php
@@ -1,10 +1,45 @@
 <?php
+require_once __DIR__ .'/Category.php';
 
 class Product{
-
+    private $name;
+    private $brand;
+    private $price;
+    private $imageUrl;
+    private $isAvailable;
     private $categiory;
 
-    public function __construct($categiory){
-        $this->categiory;
+    public function __construct($name, $brand, $price, $imageUrl, $isAvailable, $categiory){
+        $this->name = $name;
+        $this->brand = $brand;
+        $this->price = $price;
+        $this->imageUrl = $imageUrl;
+        $this->$isAvailable = $isAvailable;
+        $this->categiory = $categiory;
     }
+
+    public function getName(){
+        return $this-> name;
+    }
+
+    public function getBrand(){
+        return $this-> brand;
+    }
+    
+    public function getPrice(){
+        return $this-> price;
+    }
+    
+    public function getImageUrl(){
+        return $this-> imageUrl;
+    }
+    
+    public function getIsAvailable(){
+        return $this-> isAvailable;
+    }
+
+    public function getCategory(){
+        return $this-> categiory;
+    }
+
 }
